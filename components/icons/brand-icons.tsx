cc41c1163151f3b359b253f69d0a79151b4d0c65
Ugur Koc <ugurkoc@protonmail.com>
2026-07-11 22:55:14 +0200

    Redesign landing trust section

--- components/icons/brand-icons.tsx
+++ components/icons/brand-icons.tsx
@@ -22,6 +22,47 @@ export const Github = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
 );
 Github.displayName = "Github";
 
+export const GithubMark = forwardRef<
+  SVGSVGElement,
+  SVGProps<SVGSVGElement>
+>((props, ref) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    aria-hidden="true"
+    ref={ref}
+    {...props}
+  >
+    <path d="M12 .297a12 12 0 0 0-3.793 23.39c.6.113.82-.26.82-.577v-2.234c-3.338.726-4.042-1.416-4.042-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.73.083-.73 1.205.084 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.776.418-1.305.762-1.605-2.665-.304-5.467-1.333-5.467-5.93 0-1.31.468-2.381 1.235-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23a11.52 11.52 0 0 1 6.008 0c2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.233 1.911 1.233 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .32.216.694.825.576A12 12 0 0 0 12 .297Z" />
+  </svg>
+));
+GithubMark.displayName = "GithubMark";
+
+export const MicrosoftMark = forwardRef<
+  SVGSVGElement,
+  SVGProps<SVGSVGElement>
+>((props, ref) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={21}
+    height={21}
+    viewBox="0 0 21 21"
+    fill="none"
+    aria-hidden="true"
+    ref={ref}
+    {...props}
+  >
+    <rect x="1" y="1" width="9" height="9" fill="#F25022" />
+    <rect x="11" y="1" width="9" height="9" fill="#7FBA00" />
+    <rect x="1" y="11" width="9" height="9" fill="#00A4EF" />
+    <rect x="11" y="11" width="9" height="9" fill="#FFB900" />
+  </svg>
+));
+MicrosoftMark.displayName = "MicrosoftMark";
+
 export const Linkedin = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
   (props, ref) => (
     <svg
