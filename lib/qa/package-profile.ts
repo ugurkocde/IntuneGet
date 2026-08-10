52ff35457149be4f5def6fa3a2d11ed91dce52c5
Ugur Koc <ugurkoc@protonmail.com>
2026-08-10 23:55:29 +0200

    chore(qa): pin exact MSI identity packager

--- lib/qa/package-profile.ts
+++ lib/qa/package-profile.ts
@@ -5,7 +5,7 @@ import { DEFAULT_PSADT_CONFIG, type PSADTConfig } from '@/types/psadt';
 
 export const QA_PSADT_TOOLCHAIN = {
   packagerRepository: 'ugurkocde/IntuneGet',
-  packagerCommit: 'de49775e759b693b92db09bc99aa116f197c4850',
+  packagerCommit: 'c603eab9b8de23a6b5eb466f0fd8cdf2bfd04e33',
   packagerScriptPath: '.github/scripts/Create-PSADTPackage.ps1',
   psadtVersion: '4.1.8',
   templateUrl:
