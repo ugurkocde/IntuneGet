251c405c72bcfe864e04c28640a54a04ad9a64e9
Ugur Koc <ugurkoc@protonmail.com>
2026-08-10 12:30:54 +0200

    Pin QA profiles to hardened MSI and MSIX packager

--- lib/qa/package-profile.ts
+++ lib/qa/package-profile.ts
@@ -4,7 +4,7 @@ import { DEFAULT_PSADT_CONFIG, type PSADTConfig } from '@/types/psadt';
 
 export const QA_PSADT_TOOLCHAIN = {
   packagerRepository: 'ugurkocde/IntuneGet',
-  packagerCommit: 'a113ab2ea94e6ba02c9b22aa00706317d7bd82d2',
+  packagerCommit: '2bffe7f5c3b146c98316b02235a10dde62df58e6',
   packagerScriptPath: '.github/scripts/Create-PSADTPackage.ps1',
   psadtVersion: '4.1.8',
   templateUrl:
